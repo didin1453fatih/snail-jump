40784f335d2249984430e89a975dbd007d481bd9
didin1453fatih <didin1453fatih@gmail.com>
2020-02-14 14:27:27 +0700

    add registration system

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,3 +1,4 @@
+import { user } from './../model/user.model';
 import * as grpc from 'grpc';
 
 import { RegistrationRequest, RegistrationResponse } from '../../proto_bin/user/user_pb';
@@ -8,14 +9,24 @@ class UserHandler implements IUserServer {
 
     registration = (data: grpc.ServerUnaryCall<RegistrationRequest>, callback: grpc.sendUnaryData<RegistrationResponse>): void => {
 
-
-        console.log(JSON.stringify(data.request.toObject()))
-        const reply = new RegistrationResponse();
-
-        reply.setSuccess(true)
-        reply.setMessage('oke sudah di terima')
-
-        callback(null, reply);
+        if (data.request.getPassword() === data.request.getConfirmpassword()) {
+            user.create({
+                username: data.request.getUsername(),
+                password: data.request.getPassword(),
+                email: data.request.getEmail(),
+                gender: data.request.getGender()
+            })
+            console.log(JSON.stringify(data.request.toObject()))
+            const reply = new RegistrationResponse();
+            reply.setSuccess(true)
+            reply.setMessage('Registration success')
+            return callback(null, reply);
+        } else {
+            const reply = new RegistrationResponse();
+            reply.setSuccess(false)
+            reply.setMessage('Registration Failure. Check your confirmation password')
+            return callback(null, reply);
+        }
     }
 }
 
